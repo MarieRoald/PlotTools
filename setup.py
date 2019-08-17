2d5a149df7f00db8ae1837225a00013f548b72a8
Marie Roald <roald.marie@gmail.com>
2019-08-17 19:12:15 +0200

    Renamed package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 setup(
-    name="plottools",
+    name="TensorKit-plottools",
     version="0.0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
