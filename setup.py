ea1d146c6a7a2c303e32cd9654a8813f8d39eac5
Marie Roald <roald.marie@gmail.com>
2019-11-04 21:37:25 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import find_packages
 
 setup(
     name="TensorKit-plottools",
-    version="0.0.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
